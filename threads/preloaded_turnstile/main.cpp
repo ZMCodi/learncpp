2bbbecf8d693e06e3b7ed3a1d1b8ce8e6181b504
ZMCodi Mac <zaim.mohsin@gmail.com>
2025-05-21 00:15:55 +0100

    add preloaded turnstile

---
+++ threads/preloaded_turnstile/main.cpp
@@ -0,0 +1,97 @@
+#include <vector>
+#include "../../Threads.h"
+
+// same thing as double turnstile but instead of
+// sequentially locking and unlocking, the last thread
+// preload the turnstile with however many threads need to pass
+std::counting_semaphore<5> turnstile{0};
+std::counting_semaphore<5> turnstile2{0}; // starts locked here
+
+std::mutex mutex;
+int friendsArrived{0};
+
+void themePark(char id)
+{
+    while (true)
+    {
+        std::stringstream msg;
+        // driving
+        msg << "Friend " << id << " driving to theme park\n";
+        std::cout << msg.str();
+        if (id != 'J')
+            sleep(2, 10);
+
+        // friend signal that it has arrived
+        msg.str("");
+        msg << "Friend " << id << " has arrived at theme park\n";
+        std::cout << msg.str();
+
+        {
+            std::lock_guard lock(mutex);
+            ++friendsArrived;
+
+            // after the final friend comes, they all go in the
+            // theme park through a turnstile
+            if (friendsArrived == 5)
+            {
+                // unlocks for 5 threads at once
+                turnstile.release(5);
+            }
+        }
+
+        // each thread locks, the final thread effectively locking
+        // the turnstile again
+        turnstile.acquire();
+
+        // no need to release here since turnstile is preloaded
+
+        msg.str("");
+        msg << "Friend " << id << " is in the theme park\n";
+        std::cout << msg.str();
+        if (id != 'J') // J doesn't like theme parks very much
+            sleep(2, 6);
+
+        msg.str("");
+        msg << "Friend " << id << " is done playing and waiting at the exit\n";
+        std::cout << msg.str();
+
+        {
+            std::lock_guard lock(mutex);
+            --friendsArrived;
+
+            // last thread preloads turnstile to allow 5 threads to pass
+            if (friendsArrived == 0)
+            {
+                turnstile2.release(5);
+            }
+        }
+
+        // last thread passing effectively locks the turnstile
+        turnstile2.acquire();
+
+        // day has ended, come back again tomorrow
+        if (id == 'J')
+        {
+            msg.str("");
+            std::cout << "\n\nEOD, lets come back again tomorrow\n\n\n";
+        }
+        sleep(0, 0, secs{5});
+    }
+
+}
+
+int main()
+{
+    std::vector<std::thread> friends{};
+    friends.reserve(5);
+
+    for (int i{}; i < 5; ++i)
+    {
+        friends.emplace_back(themePark, static_cast<char>(74 + i));
+    }
+
+    for (auto& f : friends)
+    {
+        f.join();
+    }
+}
